ce1d745a953205ac48669687d39922b6c9fb1d92
donekaloinaz <doneka.loinaz@alumni.mondragon.edu>
2024-05-04 10:27:01 +0200

    Error de segmentacion por threshold
    Hay que mirar la desviacion estandar, no la varianza. Estos valores son mas normales en la imagen, sin embargo la std y mean del histograma suavizado son muy grandes y se carga el threshold. Con un threshold manual se ha conseguido segmentar la copa haciendo un imclose con strel disc 100 para cerrar las venas.

--- procesado_imagenes/Optic_cup_segmentation.m
+++ procesado_imagenes/Optic_cup_segmentation.m
@@ -14,16 +14,26 @@ imshow(green_channel,[]); title('Green Channel');
 %% 
 green_mean = mean(green_channel,"all");
 double_green_channel = double(green_channel);
-green_var = var(double_green_channel(:))/1000;
-mdf_green_channel = double_green_channel-(green_mean+green_var);
+green_std = std(double_green_channel(:));
+mdf_green_channel = double_green_channel-(green_mean+green_std);
 figure
 mdf_green_channel = uint8(mdf_green_channel);
 imshow(mdf_green_channel,[])
 gauss = gausswin(100,8.25); %as per document, window size is 100 and standar deviation is 6, so by the formula alfa = (N-1)/(2*std) alfa is 8.25
+gauss = gauss/sum(gauss); %normalized 
 green_histogram = imhist(mdf_green_channel);
 resultedhist = conv(green_histogram,gauss,'same');
-var_hist = var(resultedhist);
+figure
+subplot(121)
+plot(green_histogram); title('Original histogram');
+subplot(122)
+plot(resultedhist); title('Smoothed histogram');
+std_hist = std(resultedhist);
 mean_hist = mean(resultedhist);
-green_threhsold = mean_hist + 2*var_hist + 2*green_var + green_mean;
+green_threhsold = mean_hist + 2*std_hist + 2*green_std + green_mean;
 figure
 imshow(mdf_green_channel>green_threhsold,[])
+kernel = strel('disk',100);
+optic_cup_seg = imclose(mdf_green_channel>green_threhsold,kernel);
+figure
+imshow(optic_cup_seg,[])
