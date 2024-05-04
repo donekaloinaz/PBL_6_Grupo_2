bc8ccc11ff6228f659aec4e3be35c0816dd03f52
donekaloinaz <doneka.loinaz@alumni.mondragon.edu>
2024-05-04 16:35:43 +0200

    Otsu and adaptive threshdolding (no funciona bien todavia)

--- procesado_imagenes/Optic_cup_segmentation.m
+++ procesado_imagenes/Optic_cup_segmentation.m
@@ -7,18 +7,19 @@ im_sin_str = string(sin.image(RAN));
 I= imread(im_sin_str);
 figure
 imshow(I,[])
-%% 
+%% Select Green Channel
 figure()
 green_channel = I(:,:,2);
 imshow(green_channel,[]); title('Green Channel');
-%% 
+%% Image Preprocessing
 green_mean = mean(green_channel,"all");
 double_green_channel = double(green_channel);
 green_std = std(double_green_channel(:));
 mdf_green_channel = double_green_channel-(green_mean+green_std);
-figure
 mdf_green_channel = uint8(mdf_green_channel);
-imshow(mdf_green_channel,[])
+figure
+imshow(mdf_green_channel,[]);
+%% Histogram Smoothing and Threshold definition (by formula in citation)
 gauss = gausswin(100,8.25); %as per document, window size is 100 and standar deviation is 6, so by the formula alfa = (N-1)/(2*std) alfa is 8.25
 gauss = gauss/sum(gauss); %normalized 
 green_histogram = imhist(mdf_green_channel);
@@ -30,10 +31,19 @@ subplot(122)
 plot(resultedhist); title('Smoothed histogram');
 std_hist = std(resultedhist);
 mean_hist = mean(resultedhist);
-green_threhsold = mean_hist + 2*std_hist + 2*green_std + green_mean;
+green_threshold = mean_hist + 2*std_hist + 2*green_std + green_mean;
+%% Image binarization by theoretic threshold
 figure
-imshow(mdf_green_channel>green_threhsold,[])
+imshow(mdf_green_channel>green_threshold,[])
 kernel = strel('disk',100);
-optic_cup_seg = imclose(mdf_green_channel>green_threhsold,kernel);
+opticcup_seg_theo = imclose(mdf_green_channel>green_threshold,kernel);
 figure
-imshow(optic_cup_seg,[])
+imshow(opticcup_seg_theo); title('Theoretical binarization');
+%% Otsu method binarization
+opticcup_seg_otsu = imbinarize(mdf_green_channel,'global');
+figure
+imshow(opticcup_seg_otsu); title('Otsu binarization');
+%% Adaptive threshold binarization
+opticcup_seg_adaptive = imbinarize(mdf_green_channel,'adaptive','Sensitivity',0.001);
+figure
+imshow(opticcup_seg_adaptive); title('Adaptive binarization');
