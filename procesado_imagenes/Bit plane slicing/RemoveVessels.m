725498530d33a62fa5606800a96d34474351701f
Galibonga <igalianitz@gmail.com>
2024-05-14 10:51:44 +0200

    Remove vessels

---
+++ procesado_imagenes/Bit plane slicing/RemoveVessels.m
@@ -0,0 +1,31 @@
+function im_sin=RemoveVessels(I)
+    
+im_rgb = im2double(I);
+
+im_mask = im_rgb(:,:,2) > (20/255);
+    
+im_mask = double(imerode(im_mask, strel('disk',3)));
+    
+im_green = im_rgb(:,:,2);
+
+im_enh = adapthisteq(im_green,'numTiles',[8 8],'nBins',128);
+
+[im_enh1, ~] = replace_black_ring(im_enh,im_mask);
+
+im_gray = imcomplement(im_enh1); 
+
+se = strel('disk',10);
+im_top = imtophat(im_gray,se);
+im_top_8=im2uint8(im_top);
+im_top_8=imadjust(im_top_8);
+im_thre=im_top_8>100;
+se = strel('disk',5);
+im_thre=imopen(im_thre,se);
+se = strel('disk',7);
+im_thre=imdilate(im_thre,se);
+se = strel('disk',5);
+im_thre=imclose(im_thre,se);
+
+im_sin=inpaintCoherent(I,im_thre,"SmoothingFactor",4,"Radius",30);
+
+end
