8cab480ff2a6f04cfbf93733700a85af20471391
donekaloinaz <doneka.loinaz@alumni.mondragon.edu>
2024-05-15 16:14:03 +0200

    mas pruebas

--- procesado_imagenes/Segmentación/Bit plane slicing/green_channel_bitplaneslicing.m
+++ procesado_imagenes/Segmentación/Bit plane slicing/green_channel_bitplaneslicing.m
@@ -24,7 +24,7 @@ function bw = green_channel_bitplaneslicing(I)
     % subplot(423)
     % imshow(bit3,[])
     % subplot(424)
-    % imshow(bit4,[])
+    % imshow(bit8,[])
     % subplot(425)
     % imshow(bit5,[])
     % subplot(426)
@@ -41,19 +41,19 @@ function bw = green_channel_bitplaneslicing(I)
     for i = 1:11
         switch i
             case 1
-                seg = logical(bit6) & logical(bit7) & logical(bit5) & logical(bit4);
+                seg = logical(bit6) & logical(bit7) & logical(bit5) & logical(bit8);
                 seg = imopen(seg,se);
             case 2
                 seg = logical(bit6) & logical(bit7) & logical(bit5);
                 seg = imopen(seg,se);
             case 3
-                seg = logical(bit6) & logical(bit7) & logical(bit4);
+                seg = logical(bit6) & logical(bit7) & logical(bit8);
                 seg = imopen(seg,se);
             case 4
-                seg = logical(bit7) & logical(bit5) & logical(bit4);
+                seg = logical(bit7) & logical(bit5) & logical(bit8);
                 seg = imopen(seg,se);
             case 5
-                seg = logical(bit6) & logical(bit5) & logical(bit4);
+                seg = logical(bit6) & logical(bit5) & logical(bit8);
                 seg = imopen(seg,se);
             case 6
                 seg = logical(bit6) & logical(bit7);
@@ -62,16 +62,16 @@ function bw = green_channel_bitplaneslicing(I)
                 seg = logical(bit7) & logical(bit5);
                 seg = imopen(seg,se);
             case 8
-                seg = logical(bit7) & logical(bit4);
+                seg = logical(bit7) & logical(bit8);
                 seg = imopen(seg,se);
             case 9
                 seg = logical(bit6) & logical(bit5);
                 seg = imopen(seg,se);
             case 10
-                seg = logical(bit6) & logical(bit4);
+                seg = logical(bit6) & logical(bit8);
                 seg = imopen(seg,se);
             case 11
-                seg = logical(bit5) & logical(bit4);
+                seg = logical(bit5) & logical(bit8);
                 seg = imopen(seg,se);
         end
         statsd = regionprops(seg,'Circularity','Centroid',"Area","BoundingBox","Perimeter","Eccentricity");
@@ -109,19 +109,19 @@ function bw = green_channel_bitplaneslicing(I)
     [~,ratio_idx] = max(ratios);
     switch ratio_idx
         case 1
-            seg = logical(bit6) & logical(bit7) & logical(bit5) & logical(bit4);
+            seg = logical(bit6) & logical(bit7) & logical(bit5) & logical(bit8);
             seg = imopen(seg,se);
         case 2
             seg = logical(bit6) & logical(bit7) & logical(bit5);
             seg = imopen(seg,se);
         case 3
-            seg = logical(bit6) & logical(bit7) & logical(bit4);
+            seg = logical(bit6) & logical(bit7) & logical(bit8);
             seg = imopen(seg,se);
         case 4
-            seg = logical(bit7) & logical(bit5) & logical(bit4);
+            seg = logical(bit7) & logical(bit5) & logical(bit8);
             seg = imopen(seg,se);
         case 5
-            seg = logical(bit6) & logical(bit5) & logical(bit4);
+            seg = logical(bit6) & logical(bit5) & logical(bit8);
             seg = imopen(seg,se);
         case 6
             seg = logical(bit6) & logical(bit7);
@@ -130,16 +130,16 @@ function bw = green_channel_bitplaneslicing(I)
             seg = logical(bit7) & logical(bit5);
             seg = imopen(seg,se);
         case 8
-            seg = logical(bit7) & logical(bit4);
+            seg = logical(bit7) & logical(bit8);
             seg = imopen(seg,se);
         case 9
             seg = logical(bit6) & logical(bit5);
             seg = imopen(seg,se);
         case 10
-            seg = logical(bit6) & logical(bit4);
+            seg = logical(bit6) & logical(bit8);
             seg = imopen(seg,se);
         case 11
-            seg = logical(bit5) & logical(bit4);
+            seg = logical(bit5) & logical(bit8);
             seg = imopen(seg,se);
     end
     bw = seg;
