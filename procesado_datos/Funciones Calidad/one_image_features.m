d488b7030dc9dffb12312d19a764c035dcda09e6
donekaloinaz <doneka.loinaz@alumni.mondragon.edu>
2024-05-21 10:31:14 +0200

    resuelto predict

--- procesado_datos/Funciones Calidad/one_image_features.m
+++ procesado_datos/Funciones Calidad/one_image_features.m
@@ -10,14 +10,14 @@ brisque_vals = brisque(Ig);
 entropy_vals = entropy(Ig);
 Ifft = abs(fft(I));
 entropy_ft_vals = entropy(Ifft);
-
-Features=table();
-Features.immse = immse_vals;
-Features.psnr = psnr_vals;
-Features.ssim = ssim_vals;
-Features.std = std_vals;
-Features.brisque = brisque_vals;
-Features.entropy = entropy_vals;
-Features.ftentropy = entropy_ft_vals;
+Features = [immse_vals,psnr_vals,ssim_vals,std_vals,brisque_vals,entropy_vals,entropy_ft_vals];
+% Features=table();
+% Features.immse = immse_vals;
+% Features.psnr = psnr_vals;
+% Features.ssim = ssim_vals;
+% Features.std = std_vals;
+% Features.brisque = brisque_vals;
+% Features.entropy = entropy_vals;
+% Features.ftentropy = entropy_ft_vals;
 
 end
