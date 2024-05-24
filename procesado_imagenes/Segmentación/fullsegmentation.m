2a9f333b68a57b573797fe962173fb46eb395a71
donekaloinaz <doneka.loinaz@alumni.mondragon.edu>
2024-05-25 00:02:54 +0200

    cuplogical y centroid arreglado

--- procesado_imagenes/Segmentación/fullsegmentation.m
+++ procesado_imagenes/Segmentación/fullsegmentation.m
@@ -3,11 +3,9 @@ function [bwselected,cropped_image_novessels,cropcentre,Crop_Vessels_Removed,dis
 Crop_Vessels_Removed = RemoveVessels(cropped_image_novessels);
 disclogical = crop_image(origcentre,bwselected,bwselected);
 centre = regionprops(disclogical,'Centroid');
-cropcentre = [round(centre.Centroid(1)), round(centre.Centroid(2))];
-imforcup = uint8((double(Crop_Vessels_Removed(:,:,2))+double(Crop_Vessels_Removed(:,:,3)))/2);
-imforcupadj = imadjust(imforcup);
-imforcupadjgamma = im2uint8(imadjust(im2double(imforcupadj),[0 1],[0 1],1.5));
-cuplogical = select_last_bits_old(imforcupadjgamma);
+centre = cat(1,centre.Centroid);
+cropcentre = [round(centre(1,1)), round(centre(1,2))];
+cuplogical = getcuplogical(Crop_Vessels_Removed);
 cupdiscmask = disclogical - cuplogical;
 if length(unique(cupdiscmask)) ~= 2
     ISNTadequate = false;
