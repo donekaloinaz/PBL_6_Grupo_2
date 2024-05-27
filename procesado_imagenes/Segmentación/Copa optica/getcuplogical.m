125709a063901c057c91794f38755c2867dc7e69
donekaloinaz <doneka.loinaz@alumni.mondragon.edu>
2024-05-27 07:47:31 +0200

    Features creadas

--- procesado_imagenes/Segmentación/Copa optica/getcuplogical.m
+++ procesado_imagenes/Segmentación/Copa optica/getcuplogical.m
@@ -1,4 +1,4 @@
-function cuplogical = getcuplogical(Crop_Vessels_Removed)
+function [cuplogical,imforcupadjgamma] = getcuplogical(Crop_Vessels_Removed)
 imforcup = uint8((double(Crop_Vessels_Removed(:,:,2))+double(Crop_Vessels_Removed(:,:,3)))/2);
 imforcupadj = imadjust(imforcup);
 imforcupadjgamma = im2uint8(imadjust(im2double(imforcupadj),[0 1],[0 1],1.5));
