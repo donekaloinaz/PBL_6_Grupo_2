0f2782c63f3a434c00662eafb0a82df100fc98b4
Galibonga <igalianitz@gmail.com>
2024-06-04 11:47:59 +0200

    features

--- procesado_imagenes/Feature Extraction/get1ImageFeatures.m
+++ procesado_imagenes/Feature Extraction/get1ImageFeatures.m
@@ -1,4 +1,4 @@
-function [glaufeatures, croppedim] = get1ImageFeatures(I) 
+function [glaufeatures, croppedim,edges] = get1ImageFeatures(I) 
     gaborbank = gabor([14.8,7.4],[0 45 90 135]); %https://www.sciencedirect.com/science/article/pii/S1746809414001396?ref=pdf_download&fr=RR-2&rr=88314aa1f90303ce
     [~,~,centre,croppedim,disclogical,cuplogical,cupdiscmask,cupdiscmaskcorrected,ISNTadequate,~] = fullsegmentation(I);
     seg_features = Segmentation_features(cupdiscmask,cupdiscmaskcorrected,disclogical,cuplogical,centre,I,ISNTadequate);
@@ -9,4 +9,5 @@ function [glaufeatures, croppedim] = get1ImageFeatures(I)
     catgabor = cat(2,gabor_features.gabor1,gabor_features.gabor2,gabor_features.gabor3,gabor_features.gabor4,gabor_features.gabor5,gabor_features.gabor6,gabor_features.gabor7,gabor_features.gabor8);
     catfeatures = cat(2,catseg,catwavelet,catgabor);
     glaufeatures(1,:) = catfeatures;
+    edges=edge(cupdiscmaskcorrected);
 end
